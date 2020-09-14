e1bb10d24efda3cceab0142ea9616ba60bf4f25d
Nemanja Drakulovic <ndrakulovic@hotmail.com>
2020-09-14 23:43:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-function happyHolidays(){
-  console.log("Happy Holidays!")
+function happyHolidaysto(name) {
+  return "happy holidays " + name
 }
 
-function happyHolidaysTo(name){
-  console.log("Happy holidays,  " + name)
-}
-
-function happyHolidayTo(holiday,name){
-  console.log('Happy ' + holiday + ', ' + name + '!')
-}
-
-function holidayCountdown(days, holiday){
-  console.log("It's " + days.toString()  + " days untill " + "holiday!")
-}
-
-happyHolidays()
-
-happyHolidaysTo("Nemanja")
-
-happyHolidayTo("easter","Nemanja")
-
-holidayCountdown(90,"Christmas")
+happyHolidaysto("Nemanja")
